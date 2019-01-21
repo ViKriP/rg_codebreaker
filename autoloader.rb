0d9e3484d98a8f417a8a90e3d23d8d03b4065097
ViKriP <viktor_biz@ukr.net>
2019-01-21 15:57:43 +0200

    corrected paths in autoloader

--- autoloader.rb
+++ autoloader.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
+ROOTDIR = File.expand_path(__dir__)
+
 require 'i18n'
 require 'yaml'
 require 'terminal-table'
-require './lib/i18n_config'
-require './lib/codebreaker/version'
-require './lib/codebreaker/difficulty'
-require './lib/codebreaker/validation'
-require './lib/codebreaker/storage'
-require './lib/codebreaker/player'
-require './lib/codebreaker/game'
-require './lib/codebreaker/console'
-require './lib/codebreaker'
+require ROOTDIR + '/lib/i18n_config'
+require ROOTDIR + '/lib/codebreaker/version'
+require ROOTDIR + '/lib/codebreaker/difficulty'
+require ROOTDIR + '/lib/codebreaker/validation'
+require ROOTDIR + '/lib/codebreaker/storage'
+require ROOTDIR + '/lib/codebreaker/player'
+require ROOTDIR + '/lib/codebreaker/game'
+require ROOTDIR + '/lib/codebreaker/console'
